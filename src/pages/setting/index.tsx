2451a9e295866b1b2e3a377c0319f138f5d45e6f
Ngoc Ngo <ngocnv371@gmail.com>
2022-05-18 17:34:18 +0700

    add router links

--- src/pages/setting/index.tsx
+++ src/pages/setting/index.tsx
@@ -1,15 +1,24 @@
-import { IonContent, IonItem, IonLabel, IonList, IonPage } from '@ionic/react';
+import { IonContent, IonItem, IonLabel, IonList, IonListHeader, IonPage, IonToggle } from '@ionic/react';
 
 const Setting: React.FC = () => {
   return (
     <IonPage id="setting-page">
       <IonContent fullscreen>
         <IonList>
+          <IonListHeader>Appearance</IonListHeader>
           <IonItem>
-            <IonLabel>Setting 1</IonLabel>
+            <IonLabel>Dark Mode</IonLabel>
+            <IonToggle></IonToggle>
           </IonItem>
-          <IonItem>
-            <IonLabel>Setting 1</IonLabel>
+          <IonListHeader>Wiki</IonListHeader>
+          <IonItem routerLink='/wiki/monsters'>
+            <IonLabel>Monsters</IonLabel>
+          </IonItem>
+          <IonItem routerLink='/wiki/items'>
+            <IonLabel>Items</IonLabel>
+          </IonItem>
+          <IonItem routerLink='/wiki/mods'>
+            <IonLabel>Mods</IonLabel>
           </IonItem>
         </IonList>
       </IonContent>
